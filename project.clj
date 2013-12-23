38b379763ed579a0daa33cff1e82979f8575fdd8
Alex Bahouth <abahouth@onekingslane.com>
2013-12-22 19:03:23 -0800

    Added roxxi.utils; CHeckpoint

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject diesel "0.1.0-SNAPSHOT"
+(defproject com.onekingslane.danger/diesel "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.onekingslane.danger/clojure-common-utils "0.0.18"]])
--- src/diesel/core.clj
+++ src/diesel/core.clj
@@ -1,44 +1,12 @@
-(ns diesel.core)
+(ns diesel.core
+  (:use [roxxi.utils.print]))
 
-
-;; (interpreter [a b c]
-;;              add => :add
-;;              sub => :sub
-;;              ~div => div-expr
-
-
-
-(defn expression-dispatch
-  "It's assumed that e is a list, and thus, has a first"
-  [e op-dispatches]
-  (let [op (first e)]
-    (loop [dispatches op-dispatches]
-      (let [dispatch (first dispatches)]
-        (println dispatches)
-        (println dispatch)
-        (println op)
-        (cond (empty? dispatches)
-              (str "Unknown handler for " op)
-              (= op (first dispatch))
-              (second dispatch)
-              :else
-              (recur (next dispatches)))))))
-
-;; (interpreter [a b c]
-;;              add => :add
-;;              sub => :sub
-;;              ~div => div-expr)
-
-(defn div? [expr]
-  (and (number? expr)
-       (zero? expr)))
-
-(defn of-form? [expr proc-or-op]
-  (if (fn? proc-or-op)
-    (proc-or-op expr)
-    (and (seq? expr)
+(defn of-form? [fn-or-op expr]
+  (if (fn? fn-or-op)
+    (fn-or-op expr)
+    (and (list? expr)
          (not (empty? expr))
-         (= (first expr) proc-or-op))))
+         (= (first expr) fn-or-op))))
 
 (defn remove-dispatch-form-cruft
   "Our dispatch forms have `=>` to make them pretty.
@@ -47,38 +15,63 @@ and effectively turns `[a => b]` into `[a b]`."
   [dispatch-expr]
   [(first dispatch-expr) (last dispatch-expr)])
 
-(defmacro definterp [name formals & dispatches]
-  (let [dispatches (map remove-dispatch-form-cruft dispatches)]
+(defn ordered-expr-interp [expr dispatch-mappings]
+  (loop [op-fns=>ds dispatch-mappings]
+    (when op-fns=>ds
+      (let [op-fn=>d (first op-fns=>ds)
+            op-fn (first op-fn=>d)
+            d-val (last op-fn=>d)]
+        (if (of-form? op-fn expr)
+          d-val
+          (recur (next op-fns=>ds)))))))
+
+(defmacro definterp [name formals & dispatch-mappings]
+  (let [dispatch-mappings (vec (map remove-dispatch-form-cruft dispatch-mappings))
+        expr+formals (vec (cons 'expr formals))]
     `(defmulti ~name
-       (fn intepreter-fn ~(into [] (concat ['expr] formals))
-         (loop [forms# [~@dispatches]]
-           (when forms#
-             (let [f# (first forms#)]
-               (if (of-form? ~'expr (first f#))
-                 (last f#)
-                 (recur (next forms#)))))))
+       (fn ~'intepreter-fn ~expr+formals
+         (if (list? ~'expr)
+           (ordered-expr-interp ~'expr ~dispatch-mappings)
+           ~'expr))
        :default :unknown-operator)))
 
 
 
+(defn div? [expr]
+  (and (number? expr)
+       (zero? expr)))
+
 (macroexpand-1
-'(definterp test [x y]
+'(definterp my-interp []
    ['add => :add]
    ['sub => :sub]
    [div? => :div]))
 
-(definterp test [x y]
-   ['add => :add]
-   ['sub => :sub]
-   [div? => :div])
+(definterp my-interp []
+  ['add => :add]
+  ['sub => :sub]
+  [div? => :div])
 
-(defmethod test :add [expr x y]
-  (println expr x y))
+(defn my-interp-add-args [expr]
+  (rest expr))
 
-(defmethod test :unknown-operator [expr x y]
-  (str "Unknown handler for `" (first expr) "` when handling `" expr "`"))
+(defmethod my-interp :add [expr]
+  (apply + (map my-interp (my-interp-add-args expr))))
 
-(test '(div 6 8) :hi :hello )
+(def my-interp-sub-args my-interp-add-args)
+
+(defmethod my-interp :sub [expr]
+  (let [args (my-interp-sub-args expr)]
+    (if (= (count args) 1)
+      (my-interp (first args))
+      (apply - (map my-interp (my-interp-sub-args expr))))))
+
+(defmethod my-interp :unknown-operator [expr]
+  (str "Unknown handler for `" (first expr)
+       "` when handling `" expr "`"))
+
+(my-interp '(add 6 7))
+
 
 
 ;; (defmulti interp [e]
